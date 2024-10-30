baba422ad4fe040194a964b8a02cbbec9162b3b2
Nguyen Minh Tho <definedentity@proton.me>
2024-10-30 21:18:16 +0700

    Improve code

--- src/main/kotlin/dev/definedentity/astralgenerators/items/AGItems.kt
+++ src/main/kotlin/dev/definedentity/astralgenerators/items/AGItems.kt
@@ -6,12 +6,20 @@ import dev.definedentity.astralgenerators.utils.TextFormatting
 import net.minecraft.world.item.Item
 
 object AGItems {
-    val JARVIS =
-        AstralGenerators.REGISTRATE.item(Jarvis.ID, ::Jarvis)
-            .lang("J.A.R.V.I.S")
+    val JARVIS = item(Jarvis.ID, "J.A.R.V.I.S", ::Jarvis)
+    val MULTI_BLOCK_ASSEMBLER = item(MultiBlockAssembler.ID, ::MultiBlockAssembler)
+
+    /** Register item with default item model and custom name */
+    fun <T : Item> item(
+        name: String,
+        englishName: String,
+        itemSupplier: (Item.Properties) -> T
+    ): ItemEntry<T> {
+        return AstralGenerators.REGISTRATE.item(name, itemSupplier)
+            .lang(englishName)
             .defaultModel()
             .register()
-    val MULTI_BLOCK_ASSEMBLER = item(MultiBlockAssembler.ID, ::MultiBlockAssembler)
+    }
 
     /** Register item with default item model */
     fun <T : Item> item(name: String, itemSupplier: (Item.Properties) -> T): ItemEntry<T> {
